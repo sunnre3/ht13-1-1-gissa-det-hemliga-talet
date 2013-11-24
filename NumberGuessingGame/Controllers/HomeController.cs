1c3bea2235ac941cfd1d4a72ec39736a3d82f273
Rickard Karlsson <sunnre3@gmail.com>
2013-11-24 22:09:40 +0100

    working on views

---
+++ NumberGuessingGame/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NumberGuessingGame.Models;
+using NumberGuessingGame.ViewModels;
+
+namespace NumberGuessingGame.Controllers
+{
+    public class HomeController : Controller
+    {
+		private const string SESSION_MODEL = "SessionModel";
+
+        //
+        // GET: /
+
+        public ActionResult Index()
+        {
+			if(Session[SESSION_MODEL] == null)
+			{
+				Session[SESSION_MODEL] = new SecretNumber();
+			}
+			var sn = (SecretNumber)Session[SESSION_MODEL];
+
+
+			var model = new SecretNumberViewModel();
+			model.GuessedNumbers = sn.GuessedNumbers.ToList();
+			model.CanMakeGuess = sn.CanMakeGuess;
+
+            return View("Index", model);
+        }
+
+    }
+}
---
+++ NumberGuessingGame/ViewModels/SecretNumberViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NumberGuessingGame.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace NumberGuessingGame.ViewModels
+{
+	public class SecretNumberViewModel
+	{
+		[Required]
+		public int? Number { get; set; }
+
+		public bool CanMakeGuess { get; set; }
+
+		public List<GuessedNumber> GuessedNumbers { get; set; }
+
+		public GuessedNumber LastGuessedNumber { get; set; }
+
+		[Required]
+		[Range(1, 100)]
+		public int Guess { get; set; }
+
+		public string Title
+		{
+			get
+			{
+				if (GuessedNumbers.Count() == 0)
+				{
+					return "Första gissningen";
+				}
+
+				else if (GuessedNumbers.Count() == 1)
+				{
+					return "Andra gissningen";
+				}
+
+				else if (GuessedNumbers.Count() == 2)
+				{
+					return "Tredje gissningen";
+				}
+
+				else if (GuessedNumbers.Count() == 3)
+				{
+					return "Fjärde gissningen";
+				}
+
+				else if (GuessedNumbers.Count() == 4)
+				{
+					return "Femte gissningen";
+				}
+
+				else if (GuessedNumbers.Count() == 5)
+				{
+					return "Sjätte gissningen";
+				}
+
+				else
+				{
+					return "Sista gissningen";
+				}
+			}
+		}
+	}
+}
